d647539ef47079a8a874315cc14e251570de73cd
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2016-09-30 16:35:10 +0200

    Add initial frontend app - routes, assets

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :controls
     resources :device_types
     resources :devices
-    root 'home#index'
+    root 'devices#index', as: 'admin_root'
+  end
+
+  constraints(subdomain: 'app') do
+    root 'frontend#index', as: 'frontend_root'
   end
 end
