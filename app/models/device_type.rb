125dbec2f6f5ead2f4672d0575f4e721fce753db
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2016-10-01 16:10:37 +0200

    Destroy dependent when removing device type

--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -1,6 +1,6 @@
 class DeviceType < ActiveRecord::Base
-  has_many :control_items
-  has_many :devices
+  has_many :control_items, dependent: :destroy
+  has_many :devices, dependent: :destroy
 
   accepts_nested_attributes_for :control_items, reject_if: :all_blank, allow_destroy: true
 end
