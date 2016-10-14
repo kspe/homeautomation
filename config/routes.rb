d4c26e5867310de698ad4efd9fb9e2190cb55dfa
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2016-10-14 22:21:44 +0200

    Remove constraints from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  constraints(subdomain: 'admin') do
+  #constraints(subdomain: 'admin') do
     scope module: 'admin' do
       resources :controls
       resources :device_types, except: :show
       resources :devices, except: :show
       root 'devices#index', as: 'admin_root'
     end
-  end
+  #end
 
   constraints(subdomain: 'app') do
     scope module: 'frontend' do
